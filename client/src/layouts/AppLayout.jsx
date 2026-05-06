f8b0b6fa6e71f9caac739711a2e78769446bb33d
Jessie <jessiee_wang@berkeley.edu>
2026-05-05 18:03:16 -0700

    fix: remove double scroll causing white space on profile page

--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -18,7 +18,6 @@ export default function AppLayout() {
           display: 'flex',
           flexDirection: 'column',
           overflow: isChat ? 'hidden' : 'auto',
-          overflowY: isChat ? 'hidden' : 'scroll',
           height: '100vh',
           p: isChat ? 0 : 4,
         }}
